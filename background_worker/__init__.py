57e9b71eb15688af21cdc28fd20b9b724dafdd75
André Glatzl <andre@glatzl.me>
2018-02-17 01:57:04 +0100

    Rename celery app to app

--- background_worker/__init__.py
+++ background_worker/__init__.py
@@ -1,5 +1,5 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery_setup import app as celery_app
+from .celery_setup import app
 
-celery_app = celery_app
+app = app
