1ce703eeb503ec6dd7fbcf5ff5d1e655f0b4ebd6
André Glatzl <andre@glatzl.me>
2018-02-17 01:42:42 +0100

    Set DJANGO_SETTINGS_MODULE env for celery

--- background_worker/celery_setup.py
+++ background_worker/celery_setup.py
@@ -2,11 +2,6 @@ import os
 from celery import Celery
 from django.conf import settings
 
-
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
-
 app = Celery('api', include=['background_worker.tasks'])
 
 app.config_from_object('django.conf:settings')
