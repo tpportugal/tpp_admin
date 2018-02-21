f148ffd6bf8b55a7f5eeeb0e1424592da1f7eeab
André Glatzl <andre@glatzl.me>
2018-02-21 22:28:16 +0100

    Save PDF's to Agency folder

--- api/apps/gdrive/models.py
+++ api/apps/gdrive/models.py
@@ -3,9 +3,13 @@ from django.db import models
 from api.apps.gtfs.agencies.models import Agency
 
 
+def timetable_file_path(instance):
+    return 'maps/{0}'.format(instance.agency.slug)
+
+
 class Timetable(models.Model):
     id = models.AutoField(primary_key=True)
     map_name = models.CharField(max_length=200)
-    map_data = models.FileField(upload_to='maps', verbose_name="Ficheiro")
+    map_data = models.FileField(upload_to=timetable_file_path, verbose_name="Ficheiro")
     # Training Data
     agency = models.ForeignKey(Agency, models.DO_NOTHING)
