8c9fe61529691d550e882627909fa694e5d26f81
André Glatzl <andre@glatzl.me>
2018-02-23 19:25:35 +0100

    Redirect root url to mvp

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include
+from django.views.generic import RedirectView
 from graphene_django.views import GraphQLView
 from api.apps.gdrive.views import upload_timetable
 
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/mvp', permanent=False)),
     url(r'^api/', GraphQLView.as_view(graphiql=getattr(settings, "GRAPHQL_EDITOR", False))),
     url(r'^admin/', admin.site.urls),
     url(r'^upload/', upload_timetable),
